34bd3c80107974659d75f669b26cd4dd6582dc81
krav6 <krav6@ASUS-VIVOBOOK>
2026-03-07 13:51:56 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Models/Response.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2.Models
+{
+    internal class Response
+    {
+
+        public class Rootobject
+        {
+            public string result { get; set; }
+            public Stops_Start[] stops_start { get; set; }
+            public Stops_Finish[] stops_finish { get; set; }
+            public Schedule[] schedule { get; set; }
+            public Select_Start select_start { get; set; }
+            public Select_Finish select_finish { get; set; }
+        }
+
+        public class Select_Start
+        {
+            public string id_stop { get; set; }
+            public string id_city { get; set; }
+        }
+
+        public class Select_Finish
+        {
+            public string id_stop { get; set; }
+            public string id_city { get; set; }
+        }
+
+        public class Stops_Start
+        {
+            public string id_city { get; set; }
+            public string city_name { get; set; }
+            public string id_stop { get; set; }
+            public string stop_name { get; set; }
+            public string stop_is_change { get; set; }
+            public string stop_change_text { get; set; }
+            public Stop_Time stop_time { get; set; }
+            public object id_route { get; set; }
+            public string gps_coor { get; set; }
+            public string stop_photo { get; set; }
+        }
+
+        public class Stop_Time
+        {
+            public string _10 { get; set; }
+        }
+
+        public class Stops_Finish
+        {
+            public string id_city { get; set; }
+            public string city_name { get; set; }
+            public string id_stop { get; set; }
+            public string stop_name { get; set; }
+            public string stop_is_change { get; set; }
+            public string stop_change_text { get; set; }
+            public Stop_Time1 stop_time { get; set; }
+            public object id_route { get; set; }
+            public string gps_coor { get; set; }
+            public string stop_photo { get; set; }
+        }
+
+        public class Stop_Time1
+        {
+            public string _10 { get; set; }
+        }
+
+        public class Schedule
+        {
+            public int count { get; set; }
+            public string time { get; set; }
+            public string status { get; set; }
+            public int booking_until { get; set; }
+            public string id_route { get; set; }
+            public string route_name { get; set; }
+            public string cost_adult { get; set; }
+            public string cost_child { get; set; }
+            public string cost_transit { get; set; }
+        }
+
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,41 @@
+﻿using ConsoleApp2.Models;
+using System.Net.Http.Json;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var request = "https://smilebus.by/api/v2/route/schedule-detail?city_from_id=58&city_to_id=1&date=08.03.2026&stop_from_id=249&stop_to_id=2";
+            while (true)
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    try
+                    {
+                        // Отправка GET-запроса и получение ответа в виде строки
+                        var responseBody = await client.GetFromJsonAsync<Response.Rootobject>(request);
+                        var schedule = responseBody.schedule;
+                        var route = schedule.FirstOrDefault(o => o.time == "13:00");
+                        if (route.count > 0)
+                        {
+                            while (true)
+                            {
+                                Console.Beep();
+                            }
+                        }
+                        Console.WriteLine("resp");
+
+                        Thread.Sleep(3000);
+                    }
+                    catch (HttpRequestException e)
+                    {
+                        Console.WriteLine($"Ошибка: {e.Message}");
+                    }
+                }
+            }
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
