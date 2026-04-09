eb602878b34a29107ef52274eb83fe7e1f329e0e
Kmashaa <74793818+Kmashaa@users.noreply.github.com>
2026-04-09 11:20:47 +0300

    Actualizar Program.cs

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,4 +1,4 @@
-﻿using ConsoleApp2.Models;
+using ConsoleApp2.Models;
 using System.Net.Http.Json;
 
 namespace ConsoleApp2
@@ -7,7 +7,7 @@ namespace ConsoleApp2
     {
         static async Task Main(string[] args)
         {
-            var request = "https://smilebus.by/api/v2/route/schedule-detail?city_from_id=58&city_to_id=1&date=08.03.2026&stop_from_id=249&stop_to_id=2";
+            var request = "https://smilebus.by/api/v2/route/schedule-detail?city_from_id=1&city_to_id=58&date=10.04.2026&stop_from_id=44&stop_to_id=248";
             while (true)
             {
                 using (HttpClient client = new HttpClient())
@@ -17,7 +17,7 @@ namespace ConsoleApp2
                         // Отправка GET-запроса и получение ответа в виде строки
                         var responseBody = await client.GetFromJsonAsync<Response.Rootobject>(request);
                         var schedule = responseBody.schedule;
-                        var route = schedule.FirstOrDefault(o => o.time == "13:00");
+                        var route = schedule.FirstOrDefault(o => o.time == "18:00");
                         if (route.count > 0)
                         {
                             while (true)
